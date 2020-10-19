f0ce0c3f1c880414619344b33b32c02a0116b275
PetrifiedPanda <bfj.koehn@gmail.com>
2020-10-19 23:22:10 +0200

    Simplified SplitterEnemy::onDeath

--- SplitterEnemy.cpp
+++ SplitterEnemy.cpp
@@ -6,22 +6,13 @@ SplitterEnemy::SplitterEnemy(const sf::Vector2f& position, const sf::Vector2f& s
     : Enemy(position, size, enemyColor), generationNum_(generationNum) {}
 
 void SplitterEnemy::onDeath(World& world) {
-    const sf::Vector2f enemy1Pos(getPosition().x - getSize().x, getPosition().y + getSize().y / 2.0f);
-    const sf::Vector2f enemy2Pos(getPosition().x + getSize().x * 2.0f, getPosition().y + getSize().y / 2.0f);
+    if (generationNum_ != s_numChildSpawns) {
+        const sf::Vector2f enemy1Pos(getPosition().x - getSize().x, getPosition().y + getSize().y / 2.0f);
+        const sf::Vector2f enemy2Pos(getPosition().x + getSize().x * 2.0f, getPosition().y + getSize().y / 2.0f);
 
-    const sf::Vector2f enemySize(getSize().x / 2.0f, getSize().y / 2.0f);
-    const sf::Vector2f zeroVec(0, 0);
+        const sf::Vector2f enemySize(getSize().x / 2.0f, getSize().y / 2.0f);
+        const sf::Vector2f zeroVec(0, 0);
 
-    if (generationNum_ == s_numChildSpawns - 1) {  // This generation will be the last that spawns other Enemies
-        Enemy enemy1(enemy1Pos, enemySize, getFillColor());
-
-        if (world.canMoveInDirection(&enemy1, zeroVec))
-            world.addMoveable(std::make_unique<Enemy>(enemy1Pos, enemySize, getFillColor()));
-
-        Enemy enemy2(enemy2Pos, enemySize, getFillColor());
-        if (world.canMoveInDirection(&enemy2, zeroVec))
-            world.addMoveable(std::make_unique<Enemy>(enemy2Pos, enemySize, getFillColor()));
-    } else {
         const int nextGenNum = generationNum_ + 1;
 
         SplitterEnemy enemy1(enemy1Pos, enemySize, getFillColor(), nextGenNum);
