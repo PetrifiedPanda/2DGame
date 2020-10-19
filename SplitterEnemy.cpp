3f096bbd010d91b41d978b1cabe0d9b5570174af
PetrifiedPanda <bfj.koehn@gmail.com>
2020-10-19 19:10:18 +0200

    Made the splitter enemies be able to split into more splitter enemies, up to a limit (Because indefinitely spawning Splitter enemies would not be a great idea)

--- SplitterEnemy.cpp
+++ SplitterEnemy.cpp
@@ -1,27 +1,35 @@
 #include "SplitterEnemy.h"
 
-SplitterEnemy::SplitterEnemy() : Enemy() {}
-SplitterEnemy::SplitterEnemy(const sf::Vector2f& position, const sf::Vector2f& size, sf::Color enemyColor)
-    : Enemy(position, size, enemyColor) {}
+SplitterEnemy::SplitterEnemy(int generationNum) : Enemy(), generationNum_(generationNum) {}
+
+SplitterEnemy::SplitterEnemy(const sf::Vector2f& position, const sf::Vector2f& size, sf::Color enemyColor, int generationNum)
+    : Enemy(position, size, enemyColor), generationNum_(generationNum) {}
 
 void SplitterEnemy::onDeath(World& world) {
-    Enemy enemy1(sf::Vector2f(getPosition().x - getSize().x, getPosition().y + getSize().y / 2.0f),
-                 sf::Vector2f(getSize().x / 2.0f,
-                              getSize().y / 2.0f),
-                 getFillColor());
+    const sf::Vector2f enemy1Pos(getPosition().x - getSize().x, getPosition().y + getSize().y / 2.0f);
+    const sf::Vector2f enemy2Pos(getPosition().x + getSize().x * 2.0f, getPosition().y + getSize().y / 2.0f);
+
+    const sf::Vector2f enemySize(getSize().x / 2.0f, getSize().y / 2.0f);
     const sf::Vector2f zeroVec(0, 0);
-    if (world.canMoveInDirection(&enemy1, zeroVec))
-        world.addMoveable(std::make_unique<Enemy>(sf::Vector2f(getPosition().x - getSize().x, getPosition().y + getSize().y / 2.0f),
-                                                  sf::Vector2f(getSize().x / 2.0f,
-                                                               getSize().y / 2.0f),
-                                                  getFillColor()));
-    Enemy enemy2 = Enemy(sf::Vector2f(getPosition().x + getSize().x * 2.0f, getPosition().y + getSize().y / 2.0f),
-                         sf::Vector2f(getSize().x / 2.0f,
-                                      getSize().y / 2.0f),
-                         getFillColor());
-    if (world.canMoveInDirection(&enemy2, zeroVec))
-        world.addMoveable(std::make_unique<Enemy>(sf::Vector2f(getPosition().x + getSize().x * 2.0f, getPosition().y + getSize().y / 2.0f),
-                                                  sf::Vector2f(getSize().x / 2.0f,
-                                                               getSize().y / 2.0f),
-                                                  getFillColor()));
+
+    if (generationNum_ == s_numChildSpawns - 1) {  // This generation will be the last that spawns other
+        Enemy enemy1(enemy1Pos, enemySize, getFillColor());
+
+        if (world.canMoveInDirection(&enemy1, zeroVec))
+            world.addMoveable(std::make_unique<Enemy>(enemy1Pos, enemySize, getFillColor()));
+
+        Enemy enemy2(enemy2Pos, enemySize, getFillColor());
+        if (world.canMoveInDirection(&enemy2, zeroVec))
+            world.addMoveable(std::make_unique<Enemy>(enemy2Pos, enemySize, getFillColor()));
+    } else {
+        const int nextGenNum = generationNum_ + 1;
+
+        SplitterEnemy enemy1(enemy1Pos, enemySize, getFillColor(), nextGenNum);
+        if (world.canMoveInDirection(&enemy1, zeroVec))
+            world.addMoveable(std::make_unique<SplitterEnemy>(enemy1Pos, enemySize, getFillColor(), nextGenNum));
+
+        SplitterEnemy enemy2 = SplitterEnemy(enemy2Pos, enemySize, getFillColor(), nextGenNum);
+        if (world.canMoveInDirection(&enemy2, zeroVec))
+            world.addMoveable(std::make_unique<SplitterEnemy>(enemy2Pos, enemySize, getFillColor(), nextGenNum));
+    }
 }
--- SplitterEnemy.h
+++ SplitterEnemy.h
@@ -3,9 +3,12 @@
 #include "Enemy.h"
 
 class SplitterEnemy : public Enemy {
+    static const int s_numChildSpawns = 2;
+    const int generationNum_;
+
    public:
-    SplitterEnemy();
-    SplitterEnemy(const sf::Vector2f& position, const sf::Vector2f& size, sf::Color enemyColor);
+    SplitterEnemy(int generationNum = 0);
+    SplitterEnemy(const sf::Vector2f& position, const sf::Vector2f& size, sf::Color enemyColor, int generationNum = 0);
 
     void onDeath(World& world) override;
 };
