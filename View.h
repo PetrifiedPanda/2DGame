e39f2a0444a5104e271c26cc8a3a56305b8f193a
PetrifiedPanda <bfj.koehn@gmail.com>
2020-10-21 22:04:19 +0200

    Removed using sf::Vector2f from View

--- View.h
+++ View.h
@@ -4,8 +4,6 @@
 
 #include "Moveable.h"
 
-using sf::Vector2f;
-
 class View {
     int windowWidth_;
     int windowHeight_;
@@ -13,10 +11,10 @@ class View {
     sf::View windowView_;
     sf::View statView_;
 
-    Vector2f viewDifference_;
+    sf::Vector2f viewDifference_;
 
     Moveable* followed_;
-    Vector2f prevPlayerPos_;
+    sf::Vector2f prevPlayerPos_;
 
     float scrollingSpeed_;
     sf::Vector2f originalPlayerSize_;
@@ -31,7 +29,7 @@ class View {
     const sf::View& getWindowView() const;
     const sf::View& getStatView() const;
 
-    Vector2f getPositionInGame(const Vector2f& positionOnWindow) const;
-    Vector2f getPositionOnWindow(const Vector2f& positionInGame) const;
-    Vector2f getViewDifference() const;
+    sf::Vector2f getPositionInGame(const sf::Vector2f& positionOnWindow) const;
+    sf::Vector2f getPositionOnWindow(const sf::Vector2f& positionInGame) const;
+    sf::Vector2f getViewDifference() const;
 };
